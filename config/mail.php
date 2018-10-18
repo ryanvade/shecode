c6bc4a3890c0ac4a54ecfe6734584ada9519116c
Jared M Schooley <jaschoo@siue.edu>
2018-10-18 11:19:05 -0500

    fixing email things

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@shecode.cs.siue.edu'),
+        'name' => env('MAIL_FROM_NAME', 'SheCode-Mailer'),
     ],
 
     /*
