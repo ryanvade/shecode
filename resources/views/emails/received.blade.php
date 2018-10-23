16f8cfcee9e3520e99aa84bfabec02d3dd1318bb
Jared M Schooley <jaschoo@siue.edu>
2018-10-23 00:13:12 -0500

    updated email

--- resources/views/emails/received.blade.php
+++ resources/views/emails/received.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Your Form was received
 
-Thanks {{ $name }} for signing up. Shecode is Saturday Decemeber 2nd, 2017 at 10:30 am. Directions can be found [here](https://goo.gl/maps/y6jrS4WREQ72).
+Thanks {{ $name }} for signing up. Shecode is Saturday Decemeber 1st, 2018 at 10:00 am. Directions can be found [here](https://goo.gl/maps/y6jrS4WREQ72).
 
 We hope to see you there
 @endcomponent
